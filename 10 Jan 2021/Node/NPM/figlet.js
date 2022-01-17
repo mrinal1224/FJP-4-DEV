0fcd416f7341e42cae01fd0b71db610613e807c4
mrinal1224 <iammrinalkc@gmail.com>
2022-01-17 23:09:16 +0530

    push

---
+++ 10 Jan 2021/Node/NPM/figlet.js
@@ -0,0 +1,10 @@
+const figlet = require('figlet');
+
+figlet('Hello World!!', function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data)
+});
