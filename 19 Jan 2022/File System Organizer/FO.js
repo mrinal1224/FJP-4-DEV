fb9d551e2a53f20706b7be66b91ab9d50d2f107a
mrinal1224 <iammrinalkc@gmail.com>
2022-01-19 23:00:53 +0530

    File System Organizer 1

--- 19 Jan 2022/File System Organizer/FO.js
+++ 19 Jan 2022/File System Organizer/FO.js
@@ -7,72 +7,65 @@
 
 //js mein input Array ke from mein jaata hai and that is array is process.argv Array
 
+const fs = require("fs");
 
-const fs = require('fs')
+const path = require("path");
 
-const path = require('path')
+let input = process.argv.slice(2);
 
-let input = process.argv.slice(2)
+let inputArr = input; // [organzie , folderpath]
 
-let inputArr = input// [organzie , folderpath]
+let command = inputArr[0];
 
-let command = inputArr[0]
-
-
-switch(command){
-       case 'tree':
-              console.log('Tree Implemented')
-              break
-       case 'organize':
-              organizeFn(inputArr[1])
-              break
-       case 'help':
-              helpFn()
-              break
-       default :
-        console.log('Please enter a Valid command')
-        break;
+switch (command) {
+  case "tree":
+    console.log("Tree Implemented");
+    break;
+  case "organize":
+    organizeFn(inputArr[1]);
+    break;
+  case "help":
+    helpFn();
+    break;
+  default:
+    console.log("Please enter a Valid command");
+    break;
 }
-
-
 
 // Help Function will list all the ways by which you can run the commands of this project
-function helpFn(){
-       console.log(`List of all the commands->
+function helpFn() {
+  console.log(`List of all the commands->
                                  1)Tree - node FO.js tree <dirPath>
                                  2) organize - node FO.js organize <dirPath>
-                                 3)help - node FO.js help`)
+                                 3)help - node FO.js help`);
 }
 
+// Organize Function will organize all your target folder's files in a different folders acc to their extensions
+function organizeFn(dirPath) {
+  let destPath;
+  if (dirPath == undefined) {
+    console.log("Please enter a valid Directory Path");
+    return;
+  } // check whether directory path is passed or not and if not simply return
 
+  let doesExist = fs.existsSync(dirPath);
 
-function organizeFn(dirPath){
-      let destPath
-  if(dirPath==undefined){
-         console.log('Please enter a valid Directory Path')
-         return;
+  // this doesExist will tell the Target Folder exists or not
+
+  if (doesExist == true) {
+    destPath = path.join(dirPath, "organized_Files");
+
+    // we created a path for organized_Files Folder
+
+    // check whether in the given destPath does a folder exist with same name and if does not make a folder
+    if (fs.existsSync(destPath) == false) {
+      fs.mkdirSync(destPath);
+    } else {
+      console.log("Folder Already Exists");
+    }
+  } else {
+    console.log("Please Enter A valid Path");
   }
-
-  let doesExist = fs.existsSync(dirPath)
-
-  if(doesExist==true){
-    destPath = path.join(dirPath , 'organized_Files')
-
-     if(fs.existsSync(destPath)==false){
-            fs.mkdirSync(destPath)
-     }
-
-     else{
-            console.log('Folder Already Exists')
-     }
-  }
-
 }
-
-
 
 //     D:\Batches\FJP-4\testFolder\organized_files- we are ready to create folder her
-
-
-
-
