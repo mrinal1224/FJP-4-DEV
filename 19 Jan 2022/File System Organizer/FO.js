81869a6ec44ef17a715c8628808d718a24d2edd8
mrinal1224 <iammrinalkc@gmail.com>
2022-01-24 22:34:53 +0530

    tree complete

--- 19 Jan 2022/File System Organizer/FO.js
+++ 19 Jan 2022/File System Organizer/FO.js
@@ -184,6 +184,20 @@ function treeHelper(targetPath , indent){
          let fileName = path.basename(targetPath)
          console.log(indent + "├── " + fileName)
      }
+
+     else{
+            let dirName = path.basename(targetPath)
+            console.log(indent + "└──" + dirName)
+
+            let children = fs.readdirSync(targetPath)
+
+            for(let i=0 ; i<children.length ; i++){
+              let childpath = path.join(targetPath , children[i])
+              treeHelper(childpath , indent + '\t')
+            }
+
+
+     }
 }
 
 
