168e14309dcdf9385031fe632c65bd6e32cf1023
mrinal1224 <iammrinalkc@gmail.com>
2022-01-26 20:52:50 +0530

    datatypes

---
+++ 26 Jan 2021/RefrenceDT.js
@@ -0,0 +1,21 @@
+let person ={
+       name : 'saksham',
+       age : 21
+}
+
+console.log(person , typeof(person))
+
+
+
+
+
+let arr = [1 , 2, 'a' , 'b']
+
+console.log(arr , typeof(arr))
+
+
+function greet(){
+       console.log('hello')
+}
+
+console.log(greet , typeof(greet))
---
+++ 26 Jan 2021/primitiveDT.js
@@ -0,0 +1,28 @@
+let a = 23
+
+console.log(a , typeof(a))
+
+
+let b = 2.3
+
+console.log(b , typeof(b))
+
+
+
+let c = 'I am a String'
+console.log(c , typeof(c))
+
+
+
+let d = undefined;
+console.log(d , typeof(d))
+
+
+let e = true
+
+console.log(e , typeof(e))
+
+
+
+let f = null;
+console.log(f , typeof(f))
