8ec23c5c2774f4fee88a6b620c82712b1fd65215
mrinal1224 <iammrinalkc@gmail.com>
2022-02-02 21:25:36 +0530

    request

---
+++ 31 Jan 2022/web Scraping/request.js
@@ -0,0 +1,15 @@
+const request = require('request');
+
+
+console.log('Before')
+
+request('https://www.worldometers.info/coronavirus/', cb);
+
+
+function cb (error, response, body) {
+       console.error('error:', error); // Print the error if one occurred
+       console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+       console.log('body:', body); // Print the HTML for the Google homepage.
+     }
+
+console.log('After')
