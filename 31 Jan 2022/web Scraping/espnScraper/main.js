f3f168b2cba8e27125518a4209a7faa424ecd667
mrinal1224 <iammrinalkc@gmail.com>
2022-02-04 21:37:30 +0530

    espn1

--- 31 Jan 2022/web Scraping/espnScraper/main.js
+++ 31 Jan 2022/web Scraping/espnScraper/main.js
@@ -11,8 +11,24 @@ function cb(error , response , html){
               console.log(error)
        }
        else{
-              console.log(html)
+              extractLink(html)
        }
+}
+
+
+function extractLink(html){
+   let $ = cheerio.load(html)
+
+   let anchorElem = $('a[data-hover="View All Results"]')
+
+   let link = anchorElem.attr('href')
+
+
+   let fullLink = 'https://www.espncricinfo.com/'+link
+
+   console.log(fullLink)
+
+
 }
 
 
