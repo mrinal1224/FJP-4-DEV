170e473acf0d48ac191300de610bbe8fe60f763c
mrinal1224 <iammrinalkc@gmail.com>
2022-02-16 22:56:21 +0530

    promise

---
+++ 16th Feb 2022/1_Promises/readingFiles.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+
+console.log("before");
+
+let f1p = fs.promises.readFile("f1.txt");
+
+let f2p = fs.promises.readFile("f2.txt");
+
+let f3p = fs.promises.readFile("f3.txt");
+
+function cb(data) {
+  console.log("File Data ->" + data);
+}
+
+f1p.then(cb);
+f2p.then(cb);
+f3p.then(cb);
+
+console.log("After");
