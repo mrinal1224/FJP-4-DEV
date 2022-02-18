37e8859d3b76e5bf37c99ea5cda79b364997ffba
mrinal1224 <iammrinalkc@gmail.com>
2022-02-18 21:16:24 +0530

    promise serial

---
+++ 16th Feb 2022/1_Promises/readingFilesSerially.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+
+let f1p = fs.promises.readFile("f1.txt");
+
+
+
+function cb(data) {
+  console.log("File Data-> " + data);
+  let f2p = fs.promises.readFile("f2.txt");
+  return f2p
+}
+
+function cb2(data) {
+  console.log("File Data-> " + data);
+  let f3p = fs.promises.readFile("f3.txt");
+  return f3p
+}
+
+function cb3(data) {
+  console.log("File Data-> " + data);
+}
+
+//Improvement ->
+
+f1p.then(cb).then(cb2).then(cb3).catch(function(err){
+       console.log(err)
+});
+
+
+
+
