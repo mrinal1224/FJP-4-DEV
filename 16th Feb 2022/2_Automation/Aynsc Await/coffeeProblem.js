7c9c88b66ecc249ad49ded8ee18630d5c9037c8e
mrinal1224 <iammrinalkc@gmail.com>
2022-03-02 22:43:12 +0530

    async Await

--- 16th Feb 2022/2_Automation/Aynsc Await/coffeeProblem.js
+++ 16th Feb 2022/2_Automation/Aynsc Await/coffeeProblem.js
@@ -15,14 +15,34 @@ function processOrder(order) {
   });
 }
 
-placeOrder("coffee")
-  .then(function (demand) {
-    console.log(demand);
-    let orderisProcessed = processOrder(demand);
-    return orderisProcessed;
-  })
-  .then(function (orderisProcessed) {
-    console.log(orderisProcessed);
-  }).catch(function(err){
-         console.log(err)
-  });
+
+// Promisified Solution ->
+
+// placeOrder("coffee")
+//   .then(function (demand) {
+//     console.log(demand);
+//     let orderisProcessed = processOrder(demand);
+//     return orderisProcessed;
+//   })
+//   .then(function (orderisProcessed) {
+//     console.log(orderisProcessed);
+//   }).catch(function(err){
+//          console.log(err)
+//   });
+
+
+
+// Async Await Solution ->
+
+async function serveOrder() {
+  try {
+    let orderRecieved = await placeOrder("coffee");
+    console.log(orderRecieved); // order For Coffee
+    let processedOrder = await processOrder(orderRecieved);
+    console.log(processedOrder);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+serveOrder();
